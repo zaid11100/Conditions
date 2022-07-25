8eacabf9e1d9e4f86c826f19efe5f89c817da315
zaid11100 <info.zaydali@gmail.com>
2022-07-25 10:40:51 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,71 @@
+//Control Flow
+//1.Sequential
+//2.Conditiond
+//3.Itrations
+
+//Conditions
+//1.if condition
+//2. if - else condition
+// 3. if -else if - else conditions
+// 4.Nested conditiond
+
+//1.if condition
+
+// if('true'){console.log("iam one")}
+
+// if(1){console.log("iam one")}
+
+// if('0'){console.log("iam one")}
+
+// if(0){console.log("iam one")}
+
+// if(5){console.log("iam one")}
+
+// if(null){console.log("iam one")}
+
+// if(''){console.log("iam one")}
+
+// if('0.0'){console.log("iam one")}
+
+// if(0.2){console.log("iam one")}
+ 
+
+//Operators
+// if (3<4){
+//  console.log('monkey1')
+// }
+
+// if ((0 && '[]' )){
+//   console.log("hey")}
+
+// if ((2 == '2')|((2) == 2.0)){
+//         console.log('hey1')
+// } 
+
+// if ([1,2]==[1,2]){
+//   console.log('hey2')
+// }
+
+// if ((4^2)<<(6|7)){
+//      console.log('hey2')   
+// }
+
+// var a=3,b=5
+// if (a&b | b|a) {
+//     console.log('hey3')
+// }
+
+// console.log((a&b)<<(6|7))
+
+var a=5,b=7
+if (a>=b & a<b){
+   console.log('hey2')
+}
+
+if ('Sahana' == 'Sahana'){
+  console.log('hey1')
+  }
+
+  if ('Sahana' == 'sahana'){
+      console.log('hey3')
+  }
---
+++ 2.js
@@ -0,0 +1,58 @@
+//Write a program to decide a given  number is a +ve/-ve/0
+
+// var a='4-4'
+// if (a<0){
+//     console.log("a is -ve")
+// }
+// else if (a>0){
+//     console.log("a is +ve")
+// }
+// else {
+//     console.log("a is 0")
+// }
+
+
+//Write a program to print even or odd
+// var a=7
+// if (a % 0){
+//     console.log("EVEN")
+// }
+// else{
+//     console.log("ODD")
+// }
+
+// var a=4
+// if ((a % 2)==0){
+//     console.log("even")
+// }
+// else{
+//     (console.log("odd"))
+// }
+
+
+//Write a program to decide which number is greater?
+// var a=6
+// var b=6
+// if (a>b){
+//     console.log(`${a} is greater ${b}`)
+
+// }
+// else if (a<b){
+//     console.log(`${b} is greater than ${a}`)
+// }
+// else {
+//     console.log(`${a} and ${b} are equal`)
+// }
+
+//write of program for multiples of 3 and 5 
+
+// var a=9
+// if((a % 3)==0 ){
+//     console.log("fizz")
+// }
+// else if((a % 5)==0){
+//     console.log("buzz")
+// }
+// else {
+//     console.log("inavalid")
+// }
