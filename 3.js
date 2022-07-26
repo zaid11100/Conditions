5e8e06762adbaeb907760940d91479534b652212
Mohammed Zaid Ul Ali(Samar) <109258079+zaid11100@users.noreply.github.com>
2022-07-26 10:45:26 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,33 @@
+// var a;
+// a=24
+// if ((a<0) && (a<5) && (a<10))
+// {
+//     console.log("the value is less than 0")
+// }
+// else if ((a>0) && (a<=5) && (a<10))
+// {
+//     console.log("The range of the number lies in 0-5")
+// }
+// else if ((a>0) && (a>5) && (a<=10))
+// {
+//     console.log("the range lies between 5-10")
+// }
+// else if ((a>10) && (a<=15) && (a<20))
+// {
+//      console.log("the range lies between 11-15")
+
+// }
+// else if ((a>10) && (a>15) && (a<=20))
+// {
+//     console.log("the range lies between 16-20")
+// }
+// else if ((a>20) && (a<=25) && (a<30))
+// {
+//     console.log("the range lies between 21-25")
+// }
+// else if((a>20) && (a>25) && (a<=30))
+// {
+//     console.log("the range lies between 26-30")
+// }
+// else{
+//     (console.log("the number is more than 30"
